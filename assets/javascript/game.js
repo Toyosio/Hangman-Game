2dafd99b980e2a8f864837da9f4cb70211c7720e
Toyosi Olabisi <toyosio@gmail.com>
2017-11-20 20:03:32 -0600

    added links, footer, changed border

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,17 @@
 
 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var wins; //words user guessed correctly
+var spaces; // spaces in words
+var guessesleft = 12; //guesses user has left
+var lettersguessed; // stores letters user guessed
+var counter; //keeps track of correct GUESSES
+var nextword; //next word user needs to guess after user wins/loses game
+var hint; //hint
+
+//create alphabet button
+
+var buttons = function () {
+  button = document.getElementById('id')
 }
