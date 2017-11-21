2b9d90976a91331a881287a76392a3a501cff86e
Toyosi Olabisi <toyosio@gmail.com>
2017-11-20 18:39:35 -0600

    js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,4 @@
-window.onload = function () {
+
 
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-console.log=('letters');
-
 }
