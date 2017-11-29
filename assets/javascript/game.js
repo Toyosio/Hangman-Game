7541bcb48e71c462a2370a3228431842c9fd380c
Toyosi Olabisi <toyosio@gmail.com>
2017-11-29 09:53:55 -0600

    js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,39 @@
-var gameOn = ['Long John Silver' , 'Blackbeard' , 'Jack Sparrow' , 'Captain Hook' , 'Calico Jack' , 'Captian Kidd' , 'Captain Morgan' , 'Madame Cheng' ];
+var gameOn = ['Long John Silver', 'Blackbeard', 'Jack Sparrow', 'Captain Hook', 'Calico Jack', 'Captian Kidd', 'Captain Morgan', 'Madame Cheng'];
 var wins = 0;
 var loss = 0;
-var spaces= ""; //spaces in words and correct guess
+var spaces = ""; //spaces in words and correct guess
 var incorrectLetter = [];
 var guessesLeft = 12; //guesses user has left
 var correctGuess = []; //letter guessed correctly
 var incorrectGuess = []; //letter guessed incorrectly
 var counter; //keeps track of correct guesses
-var wordToGuess =""; //word to guess
+var wordToGuess = ""; //word to guess
 var lettersInWord = []; //letters in each word
 var clue; //sentence that describes each words
 
 //set elements to innerHTML
-document.getElementById('currentWord').innerHTML = blanksAndSuccesses.join(' ');
-	document.getElementById('remainingGuesses').innerHTML = guessesLeft;
-	document.getElementById('wins').innerHTML = wins;
-	document.getElementById('loss').innerHTML = loss;
-	document.getElementById('UsersGuesses').innerHTML = incorrectGuess;
+document.getElementById('currentWord').innerHTML = spaces;
+document.getElementById('remainingGuesses').innerHTML = guessesLeft;
+document.getElementById('wins').innerHTML = wins;
+document.getElementById('loss').innerHTML = loss;
+document.getElementById('UsersGuesses').innerHTML = incorrectGuess;
 
-	//test
-	console.log(gameOn);
-	console.log(lettersInWord);
-	console.log(spaces);
-	console.log(correctGuess);
+//test
+console.log(gameOn);
+console.log(lettersInWord);
+console.log(spaces);
+console.log(correctGuess);
 
 //resets game
-function reset () {
-wordToGuess = gameOn[Math.floor(Math.random() * gameOn.length)];
-lettersInWord = wordToGuess.split('');
-spaces = lettersInWord.length;
+function reset() {
+  wordToGuess = gameOn[Math.floor(Math.random() * gameOn.length)];
+  lettersInWord = wordToGuess.split('');
+  spaces = lettersInWord.length;
 
-guessesLeft = 12;
-incorrectGuess = 0;
-correctGuess = 0;
-startGame();
+  guessesLeft = 12;
+  incorrectGuess = 0;
+  correctGuess = 0;
+  startGame();
 }
 
 //Pick a random words
@@ -49,7 +49,7 @@ function startGame() {
 }
 
 //show blank spaces for words
-for(var i = ; i< spaces; i++) {
+for (var i = 0; i < spaces; i++) {
   correctGuess.push('__');
   document.getElementById('currentWord').innerHTML = correctGuess;
 }
@@ -57,36 +57,46 @@ for(var i = ; i< spaces; i++) {
 //if player guesses correct letter, show letters in spaces and letters guessed
 function playerGuesses(userKey) {
 
-if(wordToGuess.index(userKey) > -1)
-{
-  for(var i = 0; i< spaces; i++)
-  {
-    if(lettersInWord[i] === userKey)
-    {
+  if (wordToGuess.index(userKey) > -1) {
+    for (var i = 0; i < spaces; i++) {
+      if (lettersInWord[i] === userKey) {
 
-    correctGuess++;
-    spaces[i] = userKey;
-    document.getElementById('currentWord').innerHTML = spaces.join(' ');
+        correctGuess++;
+        spaces[i] = userKey;
+        document.getElementById('currentWord').innerHTML = spaces.join(' ');
+      }
+    }
+    console.log(spaces);
   }
+
+  //if player guesses incorrect letter show letter in user guesses; for each letter guessed take decrease guesses left
+  else {
+    incorrectLetter.push(userKey);
+    guessesLeft--;
+    document.getElementById('remainingGuesses').innerHTML = guessesLeft;
+    document.getElementById('UsersGuesses').innerHTML = incorrectGuess;
+    console.log('Incorrect Letters = ' + wrongLetters);
+    console.log('Guesses left are = ' + guessesLeft)
+  }
+
+  //if player guesses word before player runs out of guesses add one wins play happy pirate music
+  function calculate() {
+    if (counter === spaces) {
+      wins++
+      document.getElementById('wins').innerHTML = wins;
+      alert('YO HO HO!!!');
+      reset();
+    }
+    //if player runs out of guesses before winning add one losses play dangerous pirate music
+    else if (guessesleft === 0) {
+      loss++;
+      document.getElementById('loss').innerHTML = loss;
+      alert('ARGHHH Hang im from the yardarm');
+      reset();
+    }
+
+  }
+
+  startGame();
 }
-console.log(spaces);
-}
-
-//if player guesses incorrect letter show letter in user guesses; for each letter guessed take decrease guesses left
-else {
-  incorrectLetter.push(userKey);
-  guessesLeft--;
-  document.getElementById('remainingGuesses').innerHTML = guessesLeft;
-  document.getElementById('UsersGuesses').innerHTML = incorrectGuess;
-}
-}
-
-//if player guesses word before player runs out of guesses add one wins play happy pirate music
-
-
-
-//if player runs out of guesses before winning add one losses play dangerous pirate music
-
-
-
-//when we run out of words alet
+console.log(startGame);
